54f2683e43c9754097367aa1971a8cd76b6a9851
M0VVA <m0vva@protonmail.ch>
2022-01-20 22:36:45 +0000

    height and width

--- mchf-eclipse/drivers/ui/lcd/ui_gauge.c
+++ mchf-eclipse/drivers/ui/lcd/ui_gauge.c
@@ -23,14 +23,17 @@
 void CreateAnalogMeter()
 {
   // Meter outline
+    uint16_t textColor = White;
+    uint16_t backgroundColor = Black;
   	UiLcdHy28_DrawFullRect(ts.Layout->SG_IND.x,ts.Layout->SG_IND.y,ts.Layout->SG_IND.h,ts.Layout->SG_IND.w,Grey);
-  	UiLcdHy28_DrawFullRect(ts.Layout->SG_IND.x + 5,ts.Layout->SG_IND.y + 3,ts.Layout->SG_IND.h - 9,ts.Layout->SG_IND.w - 7,White);
+  	UiLcdHy28_DrawFullRect(ts.Layout->SG_IND.x + 5,ts.Layout->SG_IND.y + 3,ts.Layout->SG_IND.h - 9,ts.Layout->SG_IND.w - 7,backgroundColor);
     // tft.fillRect(0, 0, 239, 126, ILI9341_GREY);
     // tft.fillRect(5, 3, 230, 119, ILI9341_WHITE);
-
+    
 //   tft.setTextColor(ILI9341_BLACK);  // Text colour
-  uint16_t textColor = Black;
-  uint16_t backgroundColor = White;
+
+  uint16_t width = ts.Layout->SG_IND.w - 7;
+  uint16_t height = ts.Layout->SG_IND.h - 9;
   
   // Draw ticks every 5 degrees from -50 to +50 degrees (100 deg. FSD swing)
   for (int i = -50; i < 51; i += 5) {
@@ -40,18 +43,18 @@ void CreateAnalogMeter()
     // Coodinates of tick to draw
     float sx = cos((i - 90) * 0.0174532925);
     float sy = sin((i - 90) * 0.0174532925);
-    uint16_t x0 = sx * (100 + tl) + 120;
-    uint16_t y0 = sy * (100 + tl) + 140;
-    uint16_t x1 = sx * 100 + 120;
-    uint16_t y1 = sy * 100 + 140;
+    uint16_t x0 = sx * (100 + tl) + width;
+    uint16_t y0 = sy * (100 + tl) + height;
+    uint16_t x1 = sx * 100 + width;
+    uint16_t y1 = sy * 100 + height;
     
     // Coordinates of next tick for zone fill
     float sx2 = cos((i + 5 - 90) * 0.0174532925);
     float sy2 = sin((i + 5 - 90) * 0.0174532925);
-    int x2 = sx2 * (100 + tl) + 120;
-    int y2 = sy2 * (100 + tl) + 140;
-    int x3 = sx2 * 100 + 120;
-    int y3 = sy2 * 100 + 140;
+    int x2 = sx2 * (100 + tl) + width;
+    int y2 = sy2 * (100 + tl) + height;
+    int x3 = sx2 * 100 + width;
+    int y3 = sy2 * 100 + height;
     
     // Yellow zone limits
     //if (i >= -50 && i < 0) {
@@ -75,10 +78,10 @@ void CreateAnalogMeter()
     if (i % 25 != 0) tl = 8;
     
     // Recalculate coords incase tick lenght changed
-    x0 = sx * (100 + tl) + 120;
-    y0 = sy * (100 + tl) + 140;
-    x1 = sx * 100 + 120;
-    y1 = sy * 100 + 140;
+    x0 = sx * (100 + tl) + width;
+    y0 = sy * (100 + tl) + height;
+    x1 = sx * 100 + width;
+    y1 = sy * 100 + height;
     
     // Draw tick
     UiLcdHy28_DrawLine_ILI(x0, y0, x1, y1, Black);
@@ -86,8 +89,8 @@ void CreateAnalogMeter()
     // Check if labels should be drawn, with position tweaks
     if (i % 25 == 0) {
       // Calculate label positions
-      x0 = sx * (100 + tl + 10) + 120;
-      y0 = sy * (100 + tl + 10) + 140;
+      x0 = sx * (100 + tl + 10) + width;
+      y0 = sy * (100 + tl + 10) + height;
       switch (i / 25) {
         case -2: 
             UiLcdHy28_PrintTextCentered(x0, y0 - 12, 2, "0", textColor, backgroundColor, ts.Layout->TextMsg_font);
@@ -115,8 +118,8 @@ void CreateAnalogMeter()
     // Now draw the arc of the scale
     sx = cos((i + 5 - 90) * 0.0174532925);
     sy = sin((i + 5 - 90) * 0.0174532925);
-    x0 = sx * 100 + 120;
-    y0 = sy * 100 + 140;
+    x0 = sx * 100 + width;
+    y0 = sy * 100 + height;
     // Draw scale arc, don't draw the last part
     if (i < 50) UiLcdHy28_DrawLine_ILI(x0, y0, x1, y1, Black);
         //tft.drawLine(x0, y0, x1, y1, ILI9341_BLACK);
